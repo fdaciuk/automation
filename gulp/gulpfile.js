8d6101f76daf41b95b977c725a5890cc339e2ac7
Fernando Daciuk <f.daciuk@gmail.com>
2015-07-17 18:00:32 -0300

    Fix gulpfile.js

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -4,6 +4,7 @@ var fs = require('fs');
 var gulp = require('gulp');
 var less = require('gulp-less');
 var inquirer = require('inquirer');
+var paths = require('./paths');
 var JBOSS_PATH;
 var CA_PATH;
 
@@ -13,17 +14,31 @@ var questions = [{
   message: 'Qual o caminho do seu JBOSS?'
 },{
   type: 'input',
-  name: 'contaAzulPath',
+  name: 'caPath',
   message: 'Qual o caminho do seu ContaAzul?'
 }];
 
-function callback(answers) {
-  console.log(answers.jbossPath, answers.contaAzulPath);
-  gulp.start('watch');
+function questionsAnswered(data) {
+  return function(answers) {
+      data.JBOSS_PATH = answers.jbossPath;
+      data.CA_PATH = answers.caPath;
+
+      fs.writeFile('./automation.json', JSON.stringify(data), function(err) {
+        if(err) throw err;
+        gulp.start('watch');
+      });
+  };
 }
 
 gulp.task('get-paths', function(done) {
-  inquirer.prompt(questions, callback);
+  fs.readFile('./automation.json', function(err, data) {
+    if(err) throw err;
+    data = JSON.parse(data);
+    if(data.JBOSS_PATH && data.CA_PATH)
+      return gulp.start('watch');
+
+    inquirer.prompt(questions, questionsAnswered(data));
+  });
 });
 
 gulp.task( 'watch', [ 'less' ], function() {
